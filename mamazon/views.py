57bfebf0c48309802b7c636ed52110f546cd0c6e
Fumi <52829191+Te-kito@users.noreply.github.com>
2020-04-20 12:56:14 +0900

    lesson05

--- mamazon/views.py
+++ mamazon/views.py
@@ -1,5 +1,22 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Product
 
 
 class Home(TemplateView):
     template_name = 'mamazon/home.html'
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'mamazon/list.html'
+
+    def get_queryset(self):
+        # 全ての商品をまず入れる
+        queryset = Product.objects.all()
+        # もしクエリが検索バーのゲットに入っていたら
+        if 'query' in self.request.GET:
+            # qs<- 自分の検索したワード
+            qs = self.request.GET['query']
+            # querysetにフィルターをかけてやる
+            queryset = queryset.filter(name__contains=qs)
+        return queryset
